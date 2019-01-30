cccdb9f3d61675182c8247cac8dc69fbd0a2fb86
Anton Karpov <awd.com.ua@gmail.com>
2019-01-30 07:13:45 +0200

    Added the Swagger supports

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,6 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
